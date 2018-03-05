78b944e0437ead34b53ae5d75d9aab3067f938e0
Gahfy <g.herfray@gahfy.net>
2018-03-05 13:15:27 +0100

    Switching from class to object for ContextModule

--- app/src/main/kotlin/net/gahfy/feedme/injection/module/ContextModule.kt
+++ app/src/main/kotlin/net/gahfy/feedme/injection/module/ContextModule.kt
@@ -12,28 +12,26 @@ import net.gahfy.feedme.base.BaseView
 @Module
 // Safe here as we are dealing with a Dagger 2 module
 @Suppress("unused")
-class ContextModule {
-    companion object {
-        /**
-         * Provides the Context
-         * @param baseView the BaseView used to provides the context
-         * @return the Context to be provided
-         */
-        @Provides
-        @JvmStatic
-        internal fun provideContext(baseView: BaseView): Context {
-            return baseView.getContext()
-        }
+object ContextModule {
+    /**
+     * Provides the Context
+     * @param baseView the BaseView used to provides the context
+     * @return the Context to be provided
+     */
+    @Provides
+    @JvmStatic
+    internal fun provideContext(baseView: BaseView): Context {
+        return baseView.getContext()
+    }
 
-        /**
-         * Provides the Application Context
-         * @param context Context in which the application is running
-         * @return the Application Context to be provided
-         */
-        @Provides
-        @JvmStatic
-        internal fun provideApplication(context: Context): Application {
-            return context.applicationContext as Application
-        }
+    /**
+     * Provides the Application Context
+     * @param context Context in which the application is running
+     * @return the Application Context to be provided
+     */
+    @Provides
+    @JvmStatic
+    internal fun provideApplication(context: Context): Application {
+        return context.applicationContext as Application
     }
 }
